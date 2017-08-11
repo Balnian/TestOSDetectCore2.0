d401531d2e7732e03d10164f2bab46307d8c751c
Francis <super_f_qc@hotmail.com>
2017-08-10 22:33:56 -0400

    Fixes

--- TestOSDetectCore2.0/Program.cs
+++ TestOSDetectCore2.0/Program.cs
@@ -8,12 +8,21 @@ namespace TestOSDetectCore2._0
         {
 #if _WINDOWS_
             Console.WriteLine("_WINDOWS_");
+
+#if (_WINDOWS_ == win10_x64)
+
+            Console.WriteLine("win10_x64");
+#endif
 #elif _OSX_
             Console.WriteLine("_OSX_");
 
 #elif _LINUX_
             Console.WriteLine("_LINUX_");
-
+#if _LINUX_ == ubuntu_14_04_x64
+            Console.WriteLine("ubuntu_14_04_x64");
+#elif _LINUX_ == ubuntu_16_04_x64
+            Console.WriteLine("ubuntu_16_04_x64");
+#endif
 #endif
 
         }
